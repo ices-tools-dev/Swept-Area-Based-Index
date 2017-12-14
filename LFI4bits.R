6ff5940208aa92932303285674d2041bdbf52835
Adriana Villamor <adriana.villamor@ices.local>
2017-12-14 12:18:41 +0100

    Clean version for review

---
+++ LFI4bits.R
@@ -0,0 +1,281 @@
+rm(list = ls())
+####################
+# LFI for BITS #
+####################
+# Purpose: To calculate swept area, length-weight parameters and best LFI time series
+# for BITS survey 
+# Authors: Scott Large, Szymon Smoliński and Adriana Villamor
+# Date: December 2017
+
+#~~~~~~~~~~~~~~~#
+# Load packages #
+#~~~~~~~~~~~~~~~#
+
+library(icesDatras)
+library(dplyr)
+library(tidyr)
+library(broom)
+library(ggplot2)
+#devtools::install_github("ices-tools-prod/icesVocab")
+library(icesVocab)
+library(reshape2)
+
+
+#~~~~~~~~~~~~~~~#
+# Download data #
+#~~~~~~~~~~~~~~~#
+
+#From 1993 to 2007, almost all data are cod, after that all 5 species are present, 
+#should not be a problem right?
+
+hh_bits <- getDATRAS(record = "HH", "BITS", years = 1991:2017, quarters = 1)
+
+hl_bits <- getDATRAS(record = "HL", "BITS", years = 1991:2017, quarters = 1)
+
+ca_bits<-getDATRAS(record="CA",survey =  "BITS", years = 1991:2017, quarters = 1)
+
+speclist <- getCodeList("SpecWoRMS")
+
+
+hl_bits <- left_join(hl_bits, 
+                     speclist %>% 
+                       select(Key, 
+                              Description),
+                     by = c("Valid_Aphia" = "Key")) %>%
+  rename(Species = Description)
+
+
+
+ca_bits <- left_join(ca_bits, 
+                     speclist %>% 
+                       select(Key, 
+                              Description),
+                     by = c("Valid_Aphia" = "Key")) %>%
+  rename(Species = Description)
+
+##Define species list includded in the LFI calculation
+LFIspecies<- c("Gadus morhua", 
+               "Platichthys flesus",
+               "Pleuronectes platessa",
+               "Scophthalmus maximus",
+               "Merlangius merlangus")
+
+ca_bits <- ca_bits%>%
+  filter(Species %in% LFIspecies)
+hl_bits <- hl_bits%>%
+  filter(Species %in% LFIspecies)
+
+# Transform LngtClass with LngtCode "." and "0" to cm!
+
+ca_bits<-rbind(ca_bits%>%filter(LngtCode%in%c(".", "0"))%>%mutate(LngtClass=LngtClass/10),
+               ca_bits%>%filter(!LngtCode%in%c(".", "0")))
+
+hl_bits<-rbind(hl_bits%>%filter(LngtCode%in%c(".", "0"))%>%mutate(LngtClass=LngtClass/10),
+               hl_bits%>%filter(!LngtCode%in%c(".", "0")))
+
+# Change -9 values to NA
+
+ca_bits[ca_bits == -9] <- NA
+
+ca_bits[ca_bits == Inf] <- NA
+
+
+##To check relation between length and weight, fit the linear models
+ca_bitslm <- ca_bits %>% 
+  group_by(Country, Ship, Year, Gear,Species)%>%
+  filter(!is.na(IndWgt), IndWgt > 0) %>%
+  do(lm = lm(log(IndWgt) ~ log(LngtClass), data = ., 
+             singular.ok = T, 
+             na.action = na.exclude))
+
+##Get the coefficients
+ca_bitslm <- ca_bitslm %>% 
+  tidy(lm)
+ca_bitslm <- ca_bitslm %>% select(term,
+                                  estimate) %>% 
+  spread(term, estimate)%>%
+  rename(intercept = `(Intercept)`, 
+         slope = `log(LngtClass)`)
+
+##Plot slopes distribution
+ca_bitslm%>%ggplot(aes(slope))+geom_histogram()+facet_wrap(~Species,scales = "free")
+
+#Only Platichthys flesus seems to have more spread slopes, but should be ok
+
+ca_bitslm%>%group_by(Year, Species)%>%summarise(medianslope=(median(slope, na.rm = T)))%>%
+  ggplot(aes(Year, medianslope,color=Species))+geom_line(size=1)+scale_color_brewer(palette = "Dark2")+theme_bw()
+
+
+#regression seems quite fine, merging the three pieces together
+
+#need to remove a common column
+
+hl_bits <- hl_bits[ ,-1]  
+hh_bits <- hh_bits[ ,-1]        
+
+bits <- left_join(hl_bits, ca_bits)
+
+#only valid and Day hauls
+bits <- left_join(bits, hh_bits) %>%
+  filter( DayNight =="D", IndWgt > 0 ,HaulVal =="V")
+
+# better this: dat <-  dat %>% mutate(x = replace(x, x<0, NA))
+
+bits[bits == -9] <- NA
+
+sum(is.na(bits$Distance)) #2195 out of 14300
+
+# Calculate distance with the coordinates
+earth_distance <- function (long1, lat1, long2, lat2) {
+  rad <- pi/180
+  a1 <- lat1 * rad
+  a2 <- long1 * rad
+  b1 <- lat2 * rad
+  b2 <- long2 * rad
+  dlon <- b2 - a2
+  dlat <- b1 - a1
+  a <- (sin(dlat/2))^2 + cos(a1) * cos(b1) * (sin(dlon/2))^2
+  c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+  R <- 6371 # Mean radius (km) of the earth
+  d <- R * c
+  return(d)
+}
+
+bits$Distance2 <- 1000*(earth_distance(bits$ShootLong, bits$ShootLat, bits$HaulLong, bits$HaulLat))
+
+#subsitute NAs in Distance for this calculation
+
+bits <- transform(bits, Distance = ifelse(!is.na(Distance), Distance, Distance2))
+
+sum(is.na(bits$Distance))
+sum(is.na(bits$Distance2))  #572 out of 14300, is that ok? 
+
+
+p <- ggplot(bits, aes(Distance, Year))
+q<- p + geom_point() 
+q
+
+p <- ggplot(bits, aes(DoorSpread, Year))
+q<- p + geom_point() 
+q
+
+#remove outliers, up to where? 
+#should I remove also Distances and DoorSpread lower than some value? 
+#what do you think would be a good lower limit? 
+
+bits <- bits[!bits$Distance > 20000, ]
+bits <- bits[!bits$DoorSpread > 600, ]
+bits$DoorSpread[bits$DoorSpread < 10] <- NA
+
+
+#Calculations of Biomass at length per km2
+
+bits$sweptarea <- bits$Netopening*bits$Distance
+
+bits$WgtAtLngt <- bits$IndWgt*bits$HLNoAtLngt
+
+bits$biomdens <- bits$WgtAtLngt/bits$sweptarea
+
+#create different LFI time series for different L<sub>LF from 20 to 50 cm
+
+  bits <- bits %>%
+    mutate(length_bin = case_when(
+      LngtClass > 0 &
+        LngtClass <= 20 ~ 20,
+      LngtClass > 20 &
+        LngtClass <= 30 ~ 30,
+      LngtClass > 30 &
+        LngtClass <= 40 ~ 40,
+      LngtClass > 40 &
+        LngtClass <= 50 ~ 50,
+      LngtClass >50 ~ 60,
+      TRUE ~ NA_real_))
+
+bits <-  bits[!is.na(bits$biomdens),]  
+
+#all this calculates the different time series of LFI= (B  for L > L<sub> lf)/Btotal
+# for each value of L<sub>lf from 20 to 50
+# probably I could make it nicer with some loops and functions,
+#but it seems to work ;-)
+
+lfi_calc <-bits %>%
+  group_by(Year, length_bin) %>%
+  summarise(lfi = sum(biomdens))
+
+ lfi_20<- lfi_calc %>%
+  group_by(Year)%>%
+  filter(length_bin > 20)%>%
+  summarise(lfi20 = sum(lfi)) 
+
+lfi_30<- lfi_calc %>%
+  group_by(Year)%>%
+  filter(length_bin > 30)%>%
+  summarise(lfi30 = sum(lfi)) 
+  
+lfi_40<- lfi_calc %>%
+  group_by(Year)%>%
+  filter(length_bin > 40)%>%
+  summarise(lfi40 = sum(lfi)) 
+ 
+lfi_50<- lfi_calc %>%
+  group_by(Year)%>%
+  filter(length_bin > 50)%>%
+  summarise(lfi50 = sum(lfi)) 
+   
+lfiperyear <- lfi_calc %>% group_by(Year) %>% summarise(lfitot= sum(lfi))  
+
+lfi_20 <- left_join(lfi_20,lfiperyear)     
+lfi_30 <- left_join(lfi_30,lfiperyear) 
+lfi_40 <- left_join(lfi_40,lfiperyear) 
+lfi_50 <- left_join(lfi_50,lfiperyear) 
+
+lfi_20$lfi20 <- lfi_20$lfi20/lfi_20$lfitot
+lfi_30$lfi30 <- lfi_30$lfi30/lfi_30$lfitot
+lfi_40$lfi40 <- lfi_40$lfi40/lfi_40$lfitot
+lfi_50$lfi50 <- lfi_50$lfi50/lfi_50$lfitot
+
+lfi_timeseries <- left_join(lfi_20,lfi_30)
+lfi_timeseries <- left_join(lfi_timeseries,lfi_40)
+lfi_timeseries <- left_join(lfi_timeseries,lfi_50)
+
+lfi_timeseries <- subset(lfi_timeseries, select = -lfitot) 
+lfi_timeseries <- melt(lfi_timeseries, id="Year")
+
+ggplot(data=lfi_timeseries,
+       aes(x=Year, y=value, colour=variable)) +
+  geom_line()
+
+#Fitting a 5th degree polynomial finction to each LFI time series 
+
+model20 <- lm(lfi_20$lfi20 ~ poly(lfi_20$Year,5))
+
+summary(model20)
+
+model30 <- lm(lfi_30$lfi30 ~ poly(lfi_30$Year,5))
+
+summary(model30)
+
+model40 <- lm(lfi_40$lfi40 ~ poly(lfi_40$Year,5))
+
+summary(model40)
+
+model50 <- lm(lfi_50$lfi50 ~ poly(lfi_50$Year,5))
+
+#Error in poly(lfi_50$Year, 5) : 
+#'degree' must be less than number of unique points
+
+#best fit is lfi when Llf = 40 very close to Llf = 30
+
+ggplot(data=lfi_20,
+       aes(x=Year, y=lfi20)) +
+  geom_point()+geom_line()
+ggplot(data=lfi_30,
+       aes(x=Year, y=lfi30)) +
+  geom_point()+geom_line()
+ggplot(data=lfi_40,
+       aes(x=Year, y=lfi40)) +
+  geom_point()+geom_line()
+ggplot(data=lfi_50,
+       aes(x=Year, y=lfi50)) +
+  geom_point()+geom_line()
+
